f75e92c0fc77a94cdd2f3cb8d9cdc40c2930cdcc
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2024-02-13 14:14:27 +0530

    Add files via upload

---
+++ AA2/app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, jsonify, redirect
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+
+thresholds = {
+            'temperatureMin': 15,
+            'temperatureMax': 20,
+            'humidityMin': 0,
+            'humidityMax': 30,
+            'luminanceMin': 0,
+            'luminanceMax': 10,
+            'gforceMin': 0,
+            'gforceMax': 1
+        }
+
+inRangeImages=["static/icecream.png",
+         "static/testtubenormal.png",
+         "static/bread.png",
+         "static/glass.gif"]
+outsideRangeImages = ["static/icecreammelt.gif",
+                      "static/testtubelight.gif",
+                      "static/breadfungus.gif",
+                      "static/glassshaking.gif"]
+
+images = ["","",""]
+
+sensorData = {}
+
+mqtt_broker_ip = "broker.emqx.io"  
+
+def on_message(client, userdata, msg):
+    global sensorData, images
+    topic = msg.topic
+    payload = msg.payload.decode('utf-8')
+    sensorData[topic] = payload
+    # Check why the values and not updating
+    if (topic == "temperature"):
+        if( float(sensorData["/Temperature"]) < thresholds['temperatureMax']):
+            images[0] = inRangeImages[0]    
+        else:
+            images[0] = outsideRangeImages[0]
+
+    if (topic == "lux"):
+        if( float(sensorData["/Lux"]) < thresholds['luminanceMax']):
+            images[1] = inRangeImages[1]
+        else :
+            images[1] = outsideRangeImages[1] 
+
+    
+    if (topic == "humidity"):
+        if(float(sensorData["/Humidity"]) > thresholds['humidityMax']):
+            images[2] = outsideRangeImages[2]
+        else:
+             images[2] = inRangeImages[2]
+
+    if (topic == "gforce"):
+        if(float(sensorData["/Gforce"]) > thresholds['gforceMax']):
+            images[3] = outsideRangeImages[3]
+        else:
+             images[3] = inRangeImages[3]
+
+    sensorData["images"] =images
+
+    print("mqtt : ", msg.payload.decode('utf-8') )
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_message = on_message
+mqtt_client.connect(mqtt_broker_ip, 1883, 60)
+mqtt_client.subscribe("/Temperature")
+mqtt_client.subscribe("/Humidity")
+mqtt_client.subscribe("/Lux")
+mqtt_client.subscribe("/Gforce")
+mqtt_client.loop_start()
+
+
+@app.route('/')
+def index():
+    global thresholds
+    return render_template('index.html', thresholds=thresholds)
+
+@app.route("/getSensorData", methods=['POST'])
+def getSensorData():
+    return jsonify(sensorData)
+
+@app.route('/setThresholds', methods=['POST'])
+def set_thresholds():
+    global thresholds
+    if request.method == 'POST':
+        temperatureMin = request.form.get('temperatureMin')
+        temperatureMax = request.form.get('temperatureMax')
+
+        humidityMin = request.form.get('humidityMin')
+        humidityMax = request.form.get('humidityMax')
+
+        luminanceMin = request.form.get('luminanceMin')
+        luminanceMax = request.form.get('luminanceMax')
+
+        gforceMin = request.form.get('gforceMin')
+        gforceMax = request.form.get('gforceMax')
+
+        thresholds = {
+            'temperatureMin': temperatureMin,
+            'temperatureMax': temperatureMax,
+            'humidityMin': humidityMin,
+            'humidityMax': humidityMax,
+            'luminanceMin': luminanceMin,
+            'luminanceMax': luminanceMax,
+            'gforceMin': gforceMin,
+            'gforceMax': gforceMax
+        }
+
+        return redirect("/")
+        
+if __name__ == '__main__':
+    app.run(debug=True)
